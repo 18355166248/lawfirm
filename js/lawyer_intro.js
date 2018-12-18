8a4fdcc9a36cb725d5e2069d33aad5ab7b2ea7e4
viola <viola@beats.net.cn>
2018-12-18 14:24:24 +0800

    1.6初步已完成

--- js/lawyer_intro.js
+++ js/lawyer_intro.js
@@ -1,21 +1,54 @@
 $(function () {
-  $.getJSON("../data/lawyer_intro.json", function (data) {
-    console.log(data)
+  $.getJSON("data/lawyer_intro.json", function (data) {
+    console.log(data);
+    //公共变量的定义
+    let now = 1 ;//初始页面显示分页数
+    let onePageNum = 6;//每次点击加载数量(分页内容数量)
+    let totalNum = data.list.length;
+    let pageCount = Math.ceil(totalNum / onePageNum); //总分页数
     $(".banner>.txt>h2").text(data.title)
     $(".banner>.txt>p:first").text(data.txt1)
     $(".banner>.txt>p:last").text(data.txt2)
-    const list = data.list
-    let html = ''
-    list.forEach(v => {
-      html += '<div class="mini-box">' +
-        '<img src="./images/lawyer_intro/'+ v.url +'.png" alt="">' +
-        '<div class="describ">' +
-        '<h3>'+ v.name +'</h3>' +
-        '<span></span>' +
-        '<p>'+ v.intro +'</p>' +
-        '</div>' +
-        '</div>'
-    })
-    $(".box").html(html)
+    if (!os.pc || $(window).width() < 768) {
+      //mob端;
+      renderActives();
+      $("#loadMore").on("click", renderActives);
+
+    } else {
+      //pc端
+      let html = ''
+      data.list.forEach(v => {
+        html += '<div class="mini-box">' +
+          '<img src="./images/lawyer_intro/' + v.url + '.png" alt="">' +
+          '<div class="describ">' +
+          '<h3>' + v.name + '</h3>' +
+          '<span></span>' +
+          '<p>' + v.intro + '</p>' +
+          '</div>' +
+          '</div>'
+      })
+      $(".box").html(html)
+    }
+    function renderActives() {
+      var html = "";
+      var begin = (now - 1) * onePageNum;
+      for (var i = begin; i < begin + onePageNum; i++) {
+        if (i < totalNum) {
+          html += '<div class="mini-box">' +
+            '<img src="./images/lawyer_intro/' + data.list[i].url + '.png" alt="">' +
+            '<div class="describ">' +
+            '<h3>' + data.list[i].name + '</h3>' +
+            '<span></span>' +
+            '<p>' + data.list[i].intro + '</p>' +
+            '</div>' +
+            '</div>'
+        }
+      }
+      $(".box").append(html);
+      now++;
+      if (pageCount < now) {
+        $("#loadMore").hide();
+      }
+    }//end func
   });
 })
